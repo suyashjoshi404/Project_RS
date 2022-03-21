086f46c3447c869b7d92a58ef61f4b714f3764a6
suyashjoshi404 <suyash4502@gmail.com>
2022-03-21 09:24:09 +0530

    Random Commit
    ...

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -5,34 +5,53 @@ using UnityEngine;
 public class EnemyCollision : MonoBehaviour
 {
     [SerializeField] GameObject DeathVFX;
-    [SerializeField] Transform ParentObject;
-    ScoreBoard scoreBoard;
+    [SerializeField] GameObject HitVFX;
     [SerializeField] int scorePerHit = 10;
+    [SerializeField] int hitPoints = 3;
+    
+    GameObject ParentGameObject;
+    ScoreBoard scoreBoard;
+    Rigidbody rb;
 
     void Start()
     {
         //using FindObjectOfType<>() to find data of type ScoreBoard and storing it in scoreBoard
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        ParentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddingRigidbody();
     }
+
+    void AddingRigidbody()
+    {
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
 
     void OnParticleCollision(GameObject other)
     {
         ProccessScore();
-        ProccessKillEnemy();
+        if(hitPoints<1)
+        {
+            ProccessKillEnemy();
+        }
     }
 
-    private void ProccessKillEnemy()
+    void ProccessScore()
     {
+        GameObject vfx = Instantiate(HitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = ParentGameObject.transform;
+        hitPoints--;
+        //calling function ScoreCount from ScoreBoard Script and incrementing score
+        scoreBoard.ScoreCount(scorePerHit);
+    }
+    void ProccessKillEnemy()
+    {
+        
         //remember to turn on play on awake
         //this means it will play the moment its brought into existence
         GameObject vfx = Instantiate(DeathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = ParentObject;
+        vfx.transform.parent = ParentGameObject.transform;
         Destroy(gameObject);
-    }
-
-    private void ProccessScore()
-    {
-        //calling function ScoreCount from ScoreBoard Script and incrementing score
-        scoreBoard.ScoreCount(scorePerHit);
     }
 }
--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
+    TMP_Text scoreDisplay;
     int score;
+
+    void Start() 
+    {
+        scoreDisplay = GetComponent<TMP_Text>();
+        scoreDisplay.text = "Start";
+    }
+
     public void ScoreCount(int ScoreToIncrease)
     {
         score += ScoreToIncrease;
-        Debug.Log($"Score is : {score}");
+        scoreDisplay.text = score.ToString();
     }
 }
